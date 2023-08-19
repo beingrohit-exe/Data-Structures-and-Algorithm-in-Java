95b1146b1b9af4426bd3b697ac722e8e5c38dbba
Rohit Parihar <rohitparih@gmail.com>
2023-08-19 20:45:18 +0530

    Graph Generation

--- Graph/GraphDataStructure.java
+++ Graph/GraphDataStructure.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class GraphDataStructure {
 
-    static class Edge{
+    private static class Edge{
         int source;
         int neighbour;
         int weight;
